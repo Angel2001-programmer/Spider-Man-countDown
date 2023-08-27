38d0885f6681838175877d0d75a974abeec1a21d
Angel2001-programmer <angelwitchell2001@gmail.com>
2023-08-27 03:34:37 +0100

    setup project

---
+++ src/components/MyApp/MyApp.js
@@ -0,0 +1,13 @@
+import styles from "./MyApp.module.css";
+
+const MyApp = () => {
+    return(
+        <div className={styles.myapp}>
+            <div className={styles.screen}>
+            <h1>00:00</h1>
+            </div>
+        </div>
+    )
+}
+
+export default MyApp;
